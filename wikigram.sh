2fef382539df0d71c36c579801c78b57ec72e6a7
Norbert M <norbert.dev@gmail.com>
2015-01-18 17:55:13 +0100

    add a configuration to build a jar that includes all dependencies

--- wikigram.sh
+++ wikigram.sh
@@ -6,11 +6,5 @@ if [ "${1}" = "" ]; then
     exit 1
 fi
 
-# check the ${M2_REPO} environment variable
-if [ "${M2_REPO}" = "" ]; then
-    echo "Error: missing environment variable M2_REPO."
-    exit 1
-fi
-
 # start the main method
-java -server -cp ${M2_REPO}/org/apache/commons/commons-compress/1.2/commons-compress-1.2.jar:${M2_REPO}/org/hamcrest/hamcrest-core/1.1/hamcrest-core-1.1.jar:target/wikigram-2011.12.17.jar norbert.wikigram.WikiGram ${1}
+java -server -cp target/wikigram-2011.12.17-jar-with-dependencies.jar norbert.wikigram.WikiGram ${1}
